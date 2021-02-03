52ff7c58bb1606a5400e2a5d0ac446cf970ac815
wdingman <wes.dingman@nextcentury.com>
2021-02-03 14:01:33 -0500

    Fix stupid thing

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,17 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wjdingman1/mediforui/pkg/config"
 	"github.com/wjdingman1/mediforui/pkg/routes"
 )
 
 func main() {
-
+	conf, err := config.New()
+	if err != nil {
+		panic("-- CONFIG NOT FOUND: SHUTTING DOWN --")
+	}
 	r := gin.Default()
 	routes.LoadConfig(r)
 	routes.LoadFacets(r)
-	r.Run(strings.Join([]string{":", "3000"}, ""))
+	r.Run(strings.Join([]string{":", conf.GetString("PORT")}, ""))
 }
