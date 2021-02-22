9a014bc807f01e350a17fe1606db51a409c61119
wdingman <wes.dingman@nextcentury.com>
2021-02-21 21:10:26 -0500

    Added FacetsController

--- pkg/routes/facetsController.go
+++ pkg/routes/facetsController.go
@@ -7,20 +7,46 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"github.com/wjdingman1/mediforui/pkg/config"
-	"github.com/wjdingman1/mediforui/pkg/error"
+	e "github.com/wjdingman1/mediforui/pkg/error"
 )
+
+//
+
+// Facets configuration constants for the client
+type Facets struct {
+	Name        string `json:"name"`
+	Description string `json:"Description"`
+}
 
 // LoadFacets loads all the routes associated with the facetsController
 func LoadFacets(e *gin.Engine) {
-	e.GET("/facets", facetsHandler)
+	e.GET("/facets", configFacetsHandler)
 }
 
-func facetsHandler(c *gin.Context) {
-	conf, err := config.New()
+func configFacetsHandler(c *gin.Context) {
+	facets, err := newFacets()
 	log.Print("-- Retrieving facets list --")
 	if err != nil {
-		error.HandleErrorResponse(c, err, 400)
+		e.HandleErrorResponse(c, err, 400)
 	}
-	c.JSON(http.StatusOK, conf)
+	c.JSON(http.StatusOK, facets)
+
+}
+
+func newFacets() (*[]Facets, error) {
+	var facets *[]Facets
+	conf, err := config.New()
+	if err != nil {
+		return facets, err
+	}
+
+	// Viper.Get returns interface{}, assert to array of interface and assert element to map
+	// https://stackoverflow.com/a/59759138
+	for _, f := range conf.Get("FACETS").([]interface{}) {
+		log.Print(f.(map[string]interface{})["name"])
+
+	}
+
+	return facets, nil
 
 }
